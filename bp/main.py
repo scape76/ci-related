798253668f9c8ce0eb09814ef67cc4a2fbca43cb
Dan <danyaolekhq@gmail.com>
2024-04-12 13:50:01 +0300

    added dataset, refactor

--- bp/main.py
+++ bp/main.py
@@ -1,4 +1,4 @@
-from utils import generateNetwork, sigmoid, sigmoid_derivative
+from utils import generateNetwork, sigmoid, sigmoid_derivative, generate_dataset, var7_func
 
 
 # back propagation realization
@@ -7,29 +7,47 @@ from utils import generateNetwork, sigmoid, sigmoid_derivative
 # page 734
 # examples - set of examples, each with input vector X and output vector Y
 # network - multilayer with L layers, weights w(i,j), activation function g
-def backpropagation(examples, network):
-    for [x, y] in examples:
-        for i in range(len(network[1])):
-            neuron = network[1][i]
-            neuron['delta'] = sigmoid_derivative(neuron['output']) * (neuron['output'] - y[i])
+def backward_propagate_error(expected, network):
+    for i in range(len(network[1])):
+        neuron = network[1][i]
+        neuron['delta'] = sigmoid_derivative(neuron['output']) * (neuron['output'] - expected[i])
 
-        for i in range(len(network[0])):
-            errors = 0
-            for j in range(len(network[1])):
-                output_neuron = network[1][j]
-                errors += output_neuron['weights'][i] * output_neuron['delta']
+    for i in range(len(network[0])):
+        errors = 0
+        for j in range(len(network[1])):
+            output_neuron = network[1][j]
+            errors += output_neuron['weights'][i] * output_neuron['delta']
 
-            neuron = network[0][i]
-            neuron['delta'] = sigmoid_derivative(network[0][i]['output']) * errors
+        neuron = network[0][i]
+        neuron['delta'] = sigmoid_derivative(neuron['output']) * errors
+
     return network
 
 
+# def backward_propagate_error(network, expected):
+#     for i in reversed(range(len(network))):
+#         layer = network[i]
+#         errors = list()
+#         if i != len(network) - 1:
+#             for j in range(len(layer)):
+#                 error = 0.0
+#                 for neuron in network[i + 1]:
+#                     error += (neuron['weights'][j] * neuron['delta'])
+#                 errors.append(error)
+#         else:
+#             for j in range(len(layer)):
+#                 neuron = layer[j]
+#                 errors.append(neuron['output'] - expected[j])
+#         for j in range(len(layer)):
+#             neuron = layer[j]
+#             neuron['delta'] = errors[j] * sigmoid_derivative(neuron['output'])
+
 # update every weight in network
-def update_weights(network, learning_factor):
+def update_weights(network, inputs, learning_factor):
     for l in range(len(network)):
         for i in range(len(network[l])):
             for j in range(len(network[l][i]['weights'])):
-                network[l][i]['weights'][j] = network[l][i]['weights'][j] + network[l][i]['delta'] * learning_factor
+                network[l][i]['weights'][j] -= network[l][i]['delta'] * inputs[j] * learning_factor
 
 
 def activate(weights, inputs):
@@ -64,11 +82,33 @@ def predict(inputs, network):
     return outputs
 
 
+epochs = 10000
+
+training_amount = 24
+test_amount = 30 - training_amount
+
 if __name__ == '__main__':
-    network = generateNetwork(2, 3, 2)
-    res = predict([4, 2], network)
-    # backpropagation(
-    #     [[[4, 2], [0.5, 0.5]], [[4, 2], [0.5, 0.5]], [[4, 2], [0.5, 0.5]], [[4, 2], [0.5, 0.5]]], network)
-    # update_weights(network, 0.5)
-    # res = predict([4, 2], network)
-    # print(res)
+    network = generateNetwork(3, 3, 2)
+    [dataset, expected] = generate_dataset()
+
+    training_dataset = dataset[0:training_amount]
+
+    for i in range(epochs):
+        for i in range(training_amount):
+            # expected results
+            Y = expected[i]
+            # input values
+            X = training_dataset[i]
+            forward_propagation(X, network)
+            backward_propagate_error(Y, network)
+            update_weights(network, X, 0.1)
+
+    testing_dataset = dataset[training_amount:]
+
+    for i in range(test_amount):
+        print('-----------------------------')
+        print('inputs: ', testing_dataset[i])
+        res = predict(testing_dataset[i], network)
+        Y = expected[training_amount + i]
+        print('result: ', res)
+        print('expected: ', Y)
--- utils.py
+++ utils.py
@@ -2,7 +2,43 @@
 import math
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from random import random
+from random import random, shuffle
+
+init_args = [
+    [4, 6, 7],
+    [4, 6, 8],
+    [4, 6, 9],
+    [5, 6, 7],
+    [5, 6, 8],
+    [5, 6, 9],
+    [5, 7, 7],
+    [5, 7, 8],
+    [5, 7, 9],
+    [5, 8, 7],
+    [5, 8, 8],
+    [6, 8, 9],
+    [6, 6, 7],
+    [6, 6, 8],
+    [6, 6, 9],
+    [6, 7, 7],
+    [6, 7, 8],
+    [6, 7, 9],
+    [6, 8, 7],
+    [6, 8, 8],
+    [6, 8, 9],
+    [7, 7, 7],
+    [7, 7, 8],
+    [7, 7, 9],
+    [7, 8, 7],
+    [7, 8, 8],
+    [7, 8, 9],
+    [8, 8, 7],
+    [8, 8, 8],
+    [8, 8, 9],
+    # [8, 9, 7]
+]
+
+shuffle(init_args)
 
 
 # each neuron will have properties like weights and value
@@ -12,6 +48,7 @@ def sigmoid(x):
 
 def sigmoid_derivative(x):
     return x * (1 - x)
+
 
 def generateNetwork(p, l, k):
     network = list()
@@ -23,3 +60,45 @@ def generateNetwork(p, l, k):
     network.append(output_layer)
 
     return network
+
+
+def var7_func(x1, x2, x3):
+    return math.cos(x1) + math.cos(x2) - math.sin(x3)
+
+
+def var1_func(x1, x2, x3):
+    return x1 * x1 - x2 * x2 + x3 * x3
+
+
+def dataset_minmax(dataset):
+    stats = [[min(column), max(column)] for column in dataset]
+    return stats
+
+
+# first function for normalizing
+def normalize_dataset(dataset, minmax):
+    for row in dataset:
+        for i in range(len(row)):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+
+def generate_dataset():
+    minmax = dataset_minmax(init_args)
+    normalize_dataset(init_args, minmax)
+
+    results = []
+    for i in range(len(init_args)):
+        res = var1_func(init_args[i][0], init_args[i][1], init_args[i][2])
+        results.append(res)
+
+    average = sum(results) / len(results)
+
+    expected = []
+
+    for i in range(len(init_args)):
+        y = list()
+        y.append(results[i])
+        y.append(1 if results[i] > average else 0)
+        expected.append(y)
+
+    return [init_args, expected]
