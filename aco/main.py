d2a013946a17e420adff87a8716bdcc5adf95b61
Dan <danyaolekhq@gmail.com>
2024-04-19 22:15:08 +0300

    aco for tsp

--- aco/main.py
+++ aco/main.py
@@ -1,60 +1,89 @@
-from random import randint, random
+from random import randint, random, randrange, uniform
+from math import inf
+from utils import distances
 
 K = 20
-v = 0.2
+h = 2
+
+nodes_count = len(distances)
+
+pheromones = [[1 for _ in range(nodes_count)] for _ in range(nodes_count)]
+
+best_length = +inf
 
 
-def generate_ant_population(amount):
-    return ["ant" for _ in range(amount)]
+def calculate_probability(idx1, idx2):
+    s = sum(pow((elem + K), h) for elem in pheromones[idx1])
+    return (pow(pheromones[idx1][idx2] + K, h)) / s
 
 
-def generate_paths(amount):
-    return [{"length": randint(1, 10), "pheromone_level": 1} for _ in range(amount)]
+# def calculate_probability(idx1, idx2):
+#    return pow(pheromones[idx1][idx2], alpha) * pow(pheromones[idx1][idx2], -beta)
 
 
-def update_pheromone(path):
-    path["pheromone_level"] += K / path["length"]
+def aco_algorithm(batch_size):
+    global best_length
+
+    for i in range(nodes_count):
+        for j in range(nodes_count):
+            pheromones[i][j] *= 0.999
+
+    new_pheromones = pheromones.copy()
+
+    for _i in range(batch_size):
+        [path, distance] = get_random_path_from(17)
+        if distance < best_length:
+            best_length = distance
+        difference = distance - best_length + 0.01  # to not divide by zero
+        w = 0.01 / difference
+        for i in range(nodes_count + 1):
+            idx1 = path[i % nodes_count]
+            idx2 = path[(i + 1) % nodes_count]
+            new_pheromones[idx1][idx2] += w
+            new_pheromones[idx2][idx1] += w
+
+    for i in range(nodes_count):
+        n_sum = 0.0
+        for j in range(nodes_count):
+            if i == j:
+                continue
+            n_sum += new_pheromones[i][j]
+        for j in range(nodes_count):
+            pheromones[i][j] = 2 * new_pheromones[i][j] / n_sum
+
+    return best_length
 
 
-def evaporate(path):
-    path["pheromone_level"] *= v
+def get_random_path_from(indx):
+    path = []
+    dist = 0
+    path.append(indx)
+    current_indx = indx
+    while (len(path) < nodes_count):
+        n_sum = 0.0
+        possible_next = []
+        for n in range(nodes_count):
+            if (n in path):
+                continue
+            n_sum += calculate_probability(current_indx, n)
+            possible_next.append(n)
+        r = uniform(0.0, n_sum)
+        x = 0.0
+        for nn in possible_next:
+            x += calculate_probability(current_indx, nn)
+            if r <= x:
+                dist += distances[current_indx][nn]
+                current_indx = nn
+                path.append(nn)
+                break
+    dist += distances[current_indx][indx]
+    return [path, dist]
 
-
-# r - level of pheromone on one path
-# sum - sum of all levels
-def calculate_probability(r, sum):
-    return r / sum
-
-
-epochs = 2000
 
 if __name__ == '__main__':
-    paths = generate_paths(2)
-    ants = generate_ant_population(10)
-
-    print("paths before:", paths)
-
-    for i in range(epochs):
-        for ant in ants:
-            pheromone_sum = 0
-            for path in paths:
-                pheromone_sum += path['pheromone_level']
-
-            c1 = calculate_probability(paths[0]['pheromone_level'], pheromone_sum)
-
-            if (c1 < random()):
-                update_pheromone(paths[0])
-            else:
-                update_pheromone(paths[1])
-
-            c2 = calculate_probability(paths[0]['pheromone_level'], pheromone_sum)
-
-            if (c2 < random()):
-                update_pheromone(paths[0])
-            else:
-                update_pheromone(paths[1])
-
-            for path in paths:
-                evaporate(path)
-
-    print("paths after:", paths)
+    best_path = aco_algorithm(2000)
+    print('best path: ', best_path)
+    print(pheromones)
+    # [path, dist] = get_random_path_from(7)
+    # total_distance = sum(distances[path[i]][path[i + 1]] for i in range(len(path) - 1))
+    # print(total_distance + distances[path[len(path) - 1]][7])
