840a73002ef1c8e20ba5840cf116e288a67ffab7
Dan <danyaolekhq@gmail.com>
2024-04-20 12:49:55 +0300

    aco refactoring

--- aco/main.py
+++ aco/main.py
@@ -1,89 +1,107 @@
 from random import randint, random, randrange, uniform
 from math import inf
-from utils import distances
+from utils import distances, idx_to_name
+import numpy as np
 
-K = 20
-h = 2
+alpha = 1.0
+beta = 2.0
+
+p = 0.9
 
 nodes_count = len(distances)
 
-pheromones = [[1 for _ in range(nodes_count)] for _ in range(nodes_count)]
+pheromones = np.ones(distances.shape) / len(distances)
 
-best_length = +inf
-
-
-def calculate_probability(idx1, idx2):
-    s = sum(pow((elem + K), h) for elem in pheromones[idx1])
-    return (pow(pheromones[idx1][idx2] + K, h)) / s
+n_best = 1
 
 
-# def calculate_probability(idx1, idx2):
-#    return pow(pheromones[idx1][idx2], alpha) * pow(pheromones[idx1][idx2], -beta)
+def aco_algorithm(iterations):
+    global pheromones
+    best_path = ("", np.inf)
+
+    for i in range(iterations):
+        all_paths = generate_all_paths()
+        spread_pheromone(all_paths, n_best)
+        shortest_path = min(all_paths, key=lambda x: x[1])
+        if shortest_path[1] <= best_path[1]:
+            best_path = shortest_path
+        pheromones = pheromones * p
+    return best_path
 
 
-def aco_algorithm(batch_size):
-    global best_length
-
-    for i in range(nodes_count):
-        for j in range(nodes_count):
-            pheromones[i][j] *= 0.999
-
-    new_pheromones = pheromones.copy()
-
-    for _i in range(batch_size):
-        [path, distance] = get_random_path_from(17)
-        if distance < best_length:
-            best_length = distance
-        difference = distance - best_length + 0.01  # to not divide by zero
-        w = 0.01 / difference
-        for i in range(nodes_count + 1):
-            idx1 = path[i % nodes_count]
-            idx2 = path[(i + 1) % nodes_count]
-            new_pheromones[idx1][idx2] += w
-            new_pheromones[idx2][idx1] += w
-
-    for i in range(nodes_count):
-        n_sum = 0.0
-        for j in range(nodes_count):
-            if i == j:
-                continue
-            n_sum += new_pheromones[i][j]
-        for j in range(nodes_count):
-            pheromones[i][j] = 2 * new_pheromones[i][j] / n_sum
-
-    return best_length
+def spread_pheromone(all_paths, n_best):
+    sorted_paths = sorted(all_paths, key=lambda x: x[1])
+    for path, dist in sorted_paths[:n_best]:
+        for move in path:
+            pheromones[move] += 1.0 / distances[move]
 
 
-def get_random_path_from(indx):
+def gen_path_dist(path):
+    total_dist = 0
+    for ele in path:
+        total_dist += distances[ele]
+    return total_dist
+
+
+def generate_all_paths():
+    all_paths = []
+    for i in range(20):
+        path = generate_path(0)
+        all_paths.append((path, gen_path_dist(path)))
+    return all_paths
+
+
+def pick_move(pheromone, distance, visited):
+    pheromone = np.copy(pheromone)
+    pheromone[list(visited)] = 0
+
+    row = pheromone ** alpha * ((1.0 / distance) ** beta)
+
+    normalized_row = row / row.sum()
+    move = np.random.choice(range(nodes_count), 1, p=normalized_row)[0]
+
+    return move
+
+
+def generate_path(start):
     path = []
-    dist = 0
-    path.append(indx)
-    current_indx = indx
-    while (len(path) < nodes_count):
-        n_sum = 0.0
-        possible_next = []
-        for n in range(nodes_count):
-            if (n in path):
-                continue
-            n_sum += calculate_probability(current_indx, n)
-            possible_next.append(n)
-        r = uniform(0.0, n_sum)
-        x = 0.0
-        for nn in possible_next:
-            x += calculate_probability(current_indx, nn)
-            if r <= x:
-                dist += distances[current_indx][nn]
-                current_indx = nn
-                path.append(nn)
-                break
-    dist += distances[current_indx][indx]
-    return [path, dist]
+    visited = set()
+    visited.add(start)
+    prev = start
+    for i in range(nodes_count - 1):
+        move = pick_move(pheromones[prev], distances[prev], visited)
+        path.append((prev, move))
+        prev = move
+        visited.add(move)
+    path.append((prev, start))
+    return path
+
+
+def printRoute(path):
+    for [from_i, to_i] in path:
+        print(idx_to_name[from_i], " -> ")
+    print(idx_to_name[path[0][0]])
+
+
+def reshuffle_map(arr, target):
+    new_arr = []
+    idx = 0
+    for i in range(len(arr)):
+        if arr[i][1] == target:
+            idx = i
+
+    for i in range(idx + 1, len(distances)):
+        new_arr.append(arr[i])
+
+    for i in range(0, idx + 1):
+        new_arr.append(arr[i])
+
+    return new_arr
 
 
 if __name__ == '__main__':
-    best_path = aco_algorithm(2000)
-    print('best path: ', best_path)
-    print(pheromones)
-    # [path, dist] = get_random_path_from(7)
-    # total_distance = sum(distances[path[i]][path[i + 1]] for i in range(len(path) - 1))
-    # print(total_distance + distances[path[len(path) - 1]][7])
+    [best_path, best_length] = aco_algorithm(100)
+    res = reshuffle_map(best_path, 6)
+    print('best path: ', res)
+    print('best length: ', best_length)
+    printRoute(res)
